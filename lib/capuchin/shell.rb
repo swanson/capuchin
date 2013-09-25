bcf1693d1fb287ae9c9913c41c25a3324f95fd02
Matt Swanson <mdswanson@sep.com>
2013-09-25 12:54:32 -0400

    add sample directory and scaffold command

--- lib/capuchin/shell.rb
+++ lib/capuchin/shell.rb
@@ -15,5 +15,17 @@ module Capuchin
       puts Capuchin::Scheduler.new(email, @options).schedule
     end
 
+    def scaffold(path)
+      directory_path = File.expand_path(path, Dir.pwd)
+      
+      FileUtils.mkdir_p(directory_path)
+
+      FileUtils.cp_r sample_directory + '/.', directory_path
+    end
+
+    private
+      def sample_directory
+        File.expand_path("../sample_directory", File.dirname(__FILE__))
+      end
   end
 end
---
+++ lib/sample_directory/_emails/2014-01-01-a-sample-email.md
@@ -0,0 +1,7 @@
+---
+subject: "New Year's Resolution: Start a newsletter!"
+---
+
+Whelp. Here is a sample post for your hypothetical newsletter.
+
+Make sure you go fill in *your* information in `.env` and `_config.yml`!
